3fab22de9a3086e0b6eb5e88cfbd80172281c74c
sakanura <sakanurkha@gmail.com>
2025-12-23 13:55:07 +0700

    Menghapus spasi baris (clean code)

--- test1.py
+++ test1.py
@@ -1,20 +1,15 @@
 def generate_permutations(string):
     if len(string) == 1:
         return [string]
-    
     permutations = []
-    
     for i, char in enumerate(string):
         remaining_chars = string[:i] + string[i+1:]
         sub_permutations = generate_permutations(remaining_chars)
-        
         for sub_permutation in sub_permutations:
             permutations.append(char + sub_permutation)
     
     return permutations
-
 # Input
 input_string = "abcde"
-
 # Output
 print("All permutations of", input_string, "are:", generate_permutations(input_string))
